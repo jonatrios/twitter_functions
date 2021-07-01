96484ad7e475f34664b987ca8b14c7f78d4f8b40
unknown <jonathanrios@live.com.ar>
2021-07-01 15:22:29 -0300

    fixed functions

--- tweets_functions.py
+++ tweets_functions.py
@@ -1,17 +1,13 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Tue Jun 22 11:41:13 2021
 
-@author: Jonathan
-"""
-from twitter_keys import  api_key, api_secret_key, access_token, access_token_secret
+from twitter_keys import  api_key, api_secret_key, access_token, access_token_secret,env_name
 import tweepy
 import json
 from datetime import datetime
 from dateutil import tz
 
 
-
+# Establecer la conexion con Twitter mediante tweepy
 auth = tweepy.OAuthHandler(api_key, api_secret_key)
 
 auth.set_access_token(access_token,access_token_secret)
@@ -20,34 +16,37 @@ api = tweepy.API(auth,wait_on_rate_limit=True)
 
 
 
-
+# Trae tweets desde el time-line del usuario a traves del screen_name indicado
 def user_timeline(username,n):
     twet_list = []
-    for t in tweepy.Cursor(api.user_timeline, screen_name=username,
+    for t in tweepy.Cursor(api.user_timeline, screen_name=username, count=100,
                            tweet_mode='extended').items(n): #numero de tweets a traer
         t_json = json.dumps(t._json)
-        twet_list.append(json.loads(t_json,object_hook=date_hook)) #
+        twet_list.append(json.loads(t_json,object_hook=date_hook))
     return twet_list
 
-def tweet_seacrh(keyword,n):
+
+# Busca por palabra clave segun lenjuaje indicado
+def tweet_seacrh(keyword,lang,n):
     search_list = []
-    for t in tweepy.Cursor(api.search, q=keyword,lang='es',
+    for t in tweepy.Cursor(api.search, q=keyword,lang=lang,
                            tweet_mode='extended').items(n): #numero de tweets a traer
         t_json = json.dumps(t._json)
-        search_list.append(json.loads(t_json,object_hook=date_hook)) #
+        search_list.append(json.loads(t_json,object_hook=date_hook))
     return search_list
 
+# Trae tweets desde una fecha mendionada segun palabra clave
 def historical_search(keyword,from_date,n):
     from_date = datetime.strptime(from_date,'%d/%m/%Y %H:%M')
     from_date = datetime.strftime(from_date,'%Y%m%d%H%M')
     h_list = []
-    for t in tweepy.Cursor(api.search_full_archive,environment_name='learning', query=keyword,fromDate=from_date).items(n): #numero de tweets a traer
+    for t in tweepy.Cursor(api.search_full_archive,environment_name=env_name, query=keyword,fromDate=from_date).items(n): #numero de tweets a traer
         t_json = json.dumps(t._json)
-        h_list.append(json.loads(t_json,object_hook=date_hook)) #
+        h_list.append(json.loads(t_json,object_hook=date_hook))
     return h_list
 
 
-#helper functions
+#helper function para manejar las fechas, castea a uso horario Argentina
 def date_hook(json_dict):
     from_zone = tz.gettz('UTC')
     to_zone = tz.gettz('America/Buenos_Aires')
@@ -62,16 +61,6 @@ def date_hook(json_dict):
             pass
     return json_dict
 
-
-def str_to_datestr(string):
-    str_format_date = '%a %b %d %H:%M:%S +0000 %Y'
-    try:
-        d = datetime.strptime(string,str_format_date)
-        d = datetime.strftime(d,'%d/%m/%Y %H:%M:%S')
-        return d
-    except ValueError:
-        return string
-    
 
 
 
